6b5ea08c28571a4571f547b04436804d33c603f9
pmihango <pmihango@gmail.com>
2024-01-09 19:27:49 +0300

    added click event listener for the search icon

---
+++ script.js
@@ -0,0 +1,5 @@
+const searchIcon = document.querySelector('.search-icon')
+const searchForm = document.querySelector('.search-form')
+searchIcon.addEventListener('click', () =>{
+searchForm.classList.add('active')
+})
